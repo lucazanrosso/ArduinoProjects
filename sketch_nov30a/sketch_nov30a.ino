f9b56b2a5beb6a1e9670cdefa5fd24447ce0c60d
lucazanrosso <lucazanrosso94@gmail.com>
2016-12-01 00:27:44 +0100

    Add a general version of sketch_nov29a

--- sketch_nov30a/sketch_nov30a.ino
+++ sketch_nov30a/sketch_nov30a.ino
@@ -1,8 +1,11 @@
 // A simple sketch that turns on the LEDs gradually.
-// 4 LEDs are required.
+// n^2 LEDs are required.
 
-int myLeds[4] = {6, 9, 10, 11};
+int myLeds[] = {6, 9, 10, 11};
 int size = sizeof(myLeds) / sizeof(int);
+int divider = 1024 / size;
+int multiplier = 1024 / divider;
+int divider2 = 4 / size;
 int val = 0;
 bool go = true;
 int intensity = 0;
@@ -22,39 +25,34 @@ void loop() {
 // Using this, you consider, for example, val == 255 and 256 equals
   
 //  for (int i = 0; i < size; i++) {
-//    if (val > 255)
+//    if (val > divider - 1)
 //      analogWrite(myLeds[i], 255);
 //    else if (val < 0)
 //      analogWrite(myLeds[i], 0);
 //    else
-//      analogWrite(myLeds[i], val);
-//    val -= 256;
+//      analogWrite(myLeds[i], val / divider2);
+//    val -= divider;
 //  }
 
 // Using this, you consider, for example, val == 255 and 256 different
   
   for (int i = 0; i < size; i++) {
-    if (val > 255)
+    if (val > divider - 1)
       analogWrite(myLeds[i], 255);
     else if (val < 0)
       analogWrite(myLeds[i], 0);
     else
-      analogWrite(myLeds[i], val);
-    val -= 255;
+      analogWrite(myLeds[i], val / divider2);
+    val -= divider - 1;
   }
-
-//  for (int i = 0; i < size; i++) {
-//    analogWrite(myLeds[i], val/4);
-//    val = val / 2;
-//  }
 
   if (go)
     intensity++;
   else
     intensity--;
 //  if (intensity == 1023 || intensity == 0)
-  if (intensity == 1023 - size + 1 || intensity == 0)
+  if (intensity == (divider - 1) * multiplier || intensity == 0)
     go = !go;
  
-  delay(10);
+  delay(5);
 }
