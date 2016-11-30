bd9b8e06407507409988b0d9c7c388a8f2385375
lucazanrosso <lucazanrosso94@gmail.com>
2016-11-30 01:28:12 +0100

    Add first exercise

---
+++ sketch_nov29a/sketch_nov29a.ino
@@ -0,0 +1,43 @@
+int myLeds[4] = {6, 9, 10, 11};
+int size = sizeof(myLeds)/sizeof(int);
+int val = 0;
+bool go = true;
+int intensity = 0;
+
+void setup() {
+  Serial.begin(9600);
+  for (int i = 0; i < size; i++) {
+    pinMode(myLeds[i], OUTPUT);
+  }
+}
+
+void loop() {
+//  val = analogRead(0);
+  val = intensity;
+  Serial.println(val);
+  
+  for (int i = 0; i < size; i++) {
+    if (val > 255)
+      analogWrite(myLeds[i], 255);
+    else if (val < 0)
+      analogWrite(myLeds[i], 0);
+    else
+      analogWrite(myLeds[i], val);
+    val -= 256;
+  }
+
+//  int n = 4;
+//  for (int i = 0; i < size; i++) {
+//    analogWrite(myLeds[i], val/n);
+//    n = n * 2;
+//  }
+
+  if (go)
+    intensity++;
+  else
+    intensity--;
+  if (intensity == 1023 || intensity == 0)
+    go = !go;
+ 
+  delay(10);
+}
