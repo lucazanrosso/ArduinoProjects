015a64252eb084b2f199e3af92a56c00d777eeeb
lucazanrosso <lucazanrosso94@gmail.com>
2016-12-04 15:43:40 +0100

    Add an exercise more powerful than the dec01a

--- sketch_dec04a/sketch_dec04a.ino
+++ sketch_dec04a/sketch_dec04a.ino
@@ -1,17 +1,13 @@
 // A simple sketch that turns on the LEDs gradually.
-// If the input value is greater than 765 the light is set to the maximum.
+// If the input value is greater than 767 the light is set to the maximum.
 // If the input value is less than 255 the light is off.
 // n^2 LEDs are required.
 
 int myLeds[] = {6, 9, 10, 11};
 int size = sizeof(myLeds) / sizeof(int);
-int divider = 1024 / size;
-int multiplier = 1024 / divider;
-int divider2 = 4 / size;
-int maxVal = (divider - 1) * multiplier;
-int maxVal2 = (divider / 2 - 1) * multiplier;
-int val = 0;
-float fVal = 0;
+int maxVal = 512 / size;
+float divider = (float) 2 / size;
+float val = 0;
 bool go = true;
 int intensity = 0;
 
@@ -23,29 +19,26 @@ void setup() {
 }
 
 void loop() {
-//  val = 1023 - analogRead(0);
-  val = 1023 - intensity;
-  Serial.println(val);
-
-  if (val < maxVal / 4)
+//  val = 1023 - analogRead(0) + 1;
+  val = 1023 - intensity + 1; 
+//  Serial.println(val);
+  
+  if (val < 256)
     for (int i = 0; i < size; i++)
         analogWrite(myLeds[i], 0);
-  else if (val > maxVal / 4 * 3)
+  else if (val > 768)
     for (int i = 0; i < size; i++)
         analogWrite(myLeds[i], 255);
   else {
-    val -= maxVal / 4;
-    fVal = val;
-    fVal = fVal / (maxVal / 2) * maxVal2;
-    val = (int) fVal;
+    val -= 256;
     for (int i = 0; i < size; i++) {
-      if (val > divider / 2 - 1)
+      if (val >= maxVal)
         analogWrite(myLeds[i], 255);
-      else if (val < 0)
+      else if (val <= 1)
         analogWrite(myLeds[i], 0);
       else
-        analogWrite(myLeds[i], val / divider2 * 2);
-      val -= divider / 2 - 1;
+        analogWrite(myLeds[i], (val / divider) - 1);
+      val -= maxVal;
     }
   }
 
@@ -53,7 +46,7 @@ void loop() {
     intensity++;
   else
     intensity--;
-  if (intensity == maxVal || intensity == 0)
+  if (intensity == 1023 || intensity == 0)
     go = !go;
  
   delay(10);
