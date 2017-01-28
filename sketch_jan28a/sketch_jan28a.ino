01ae0ab7985ba8080f887845c58dd0a9ffc951b5
lucazanrosso <lucazanrosso94@gmail.com>
2017-01-29 00:50:15 +0100

    Improve exercise

--- sketch_jan28a/sketch_jan28a.ino
+++ sketch_jan28a/sketch_jan28a.ino
@@ -1,15 +1,14 @@
 int myLeds[] = {6, 9, 10, 11};
 int size = sizeof(myLeds) / sizeof(int);
+int light = 0;
 int val = 0;
 int val2 = 0;
-int val3 = 0;
-
-bool go = true;
-int intensity = 0;
-
 int led = 0;
 int led2 = 0;
 bool firstStart = true;
+
+bool go = true;
+int intensity = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -19,50 +18,49 @@ void setup() {
 }
 
 void loop() {
-  val = 1023 - analogRead(0) + 1;
-//  val = 1023 - intensity + 1; 
+//  light = 1023 - analogRead(0) + 1;
+  light = 1023 - intensity + 1; 
   led2 = led;
 
   if (firstStart) {
-    val2 = val;
-    led = 0;
+    val = light;
     while (val2 > 256) {
       led++;
-      val2 -= 256;
+      val -= 256;
     }
     firstStart = false;
   } else {
-    val2 = val - led * 256;
-    if (val2 >= 288) {
+    val = light - led * 256;
+    if (val > 319) {
       led++; 
-    } else if (val2 <= -32) {
+    } else if (val < -63) {
       led--;
     }
   }
   
-  val3 = 0;
-  if (val2 >= 256) {
+  val2 = 0;
+  if (val > 256) {
     led2++;
-    val3 = val2 - 256;
-  } else if (val2 <= 0) {
+    val2 = val - 256;
+  } else if (val < 0) {
     led2--;
-    val3 = - val2;
+    val2 = - val;
   }
 
   for (int i = 0; i < size; i++) {
     if (i == led)
       analogWrite(myLeds[i], 255);
     else if (i == led2)
-      analogWrite(myLeds[i], val3);
+      analogWrite(myLeds[i], val2);
     else
       analogWrite(myLeds[i], 0);
   }
 
-  Serial.println(val);
-  Serial.println(led);
-  Serial.println(val2);
-  Serial.println(val3);
-  Serial.println();
+//  Serial.println(light);
+//  Serial.println(led);
+//  Serial.println(val);
+//  Serial.println(val2);
+//  Serial.println();
 
   if (go)
     intensity++;
@@ -71,5 +69,5 @@ void loop() {
   if (intensity == 1023 || intensity == 0)
     go = !go;
  
-  delay(50);
+  delay(20);
 }
