9341b5774c93e6152e5077890bd65b085050e443
lucazanrosso <lucazanrosso94@gmail.com>
2017-01-29 00:03:46 +0100

    Continue to build exercise

--- sketch_jan28a/sketch_jan28a.ino
+++ sketch_jan28a/sketch_jan28a.ino
@@ -1,8 +1,3 @@
-// A simple sketch that turns on the LEDs gradually.
-// If the input value is greater than 767 the light is set to the maximum.
-// If the input value is less than 255 the light is off.
-// n^2 LEDs are required.
-
 int myLeds[] = {6, 9, 10, 11};
 int size = sizeof(myLeds) / sizeof(int);
 int val = 0;
@@ -14,6 +9,7 @@ int intensity = 0;
 
 int led = 0;
 int led2 = 0;
+bool firstStart = true;
 
 void setup() {
   Serial.begin(9600);
@@ -23,20 +19,26 @@ void setup() {
 }
 
 void loop() {
-  val = 1023 - analogRead(0) + 1;
-//  val = 1023 - intensity + 1; 
-  Serial.println(val);
+//  val = 1023 - analogRead(0) + 1;
+  val = 1023 - intensity + 1; 
+//  Serial.println(val);
 
-  led = 0;
-  val2 = val;
-  while (val2 > 256) {
-    led++;
-    val2 -= 256;
-  }
+//  val2 = 256 * led;
+//  if (val2 < val) {
+//    while (val2 > 288) {
+//          led++;
+//    }
+//  } else {
+//    while (val2 > 288) {
+//          led++;
+//    }
+//  }
+//  val3 = 256 * led;
   
 //  if (led > 0) {
 //    while (val2 > 256) {
-//      led++;
+//      if (val2 > 288)
+//        led++;
 //      val2 -= 256;
 //    }
 //  } else if ((led < size) {
@@ -45,23 +47,35 @@ void loop() {
 //      val2 -= 256;
 //    }
 //  }
+
+  if (firstStart) {
+    val2 = val;
+    led = 0;
+    while (val2 > 256) {
+      led++;
+      val2 -= 256;
+    }
+    firstStart = false;
+  } else {
+    val2 = val - led * 256;
+    if (val2 >= 288) {
+      led++; 
+    } else if (val2 <= -32) {
+      led--;
+    }
+  }
+//  Serial.println(val - val2);
   
   led2 = led;
   val3 = 0;
-  if (val2 > 224) {
+  if (val2 > 256) {
     led2++;
-    val3 = val2 - 224;
-  } else if (val2 < 32) {
+    val3 = val2 - 256;
+  } else if (val2 < 0) {
     led2--;
-    val3 = 32 - val2;
+    val3 = - val2;
   }
-//  Serial.println(val3);
-
-  if (led > led2) {
-    
-  } else if (led < led2) {
-    
-  }
+  Serial.println(val3);
   
 
   for (int i = 0; i < size; i++) {
@@ -81,5 +95,5 @@ void loop() {
   if (intensity == 1023 || intensity == 0)
     go = !go;
  
-  delay(10);
+  delay(20);
 }
