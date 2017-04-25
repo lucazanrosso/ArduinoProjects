72ac218c9759cd207e6c10bdb495c7257645c1fc
lucazanrosso <lucazanrosso94@gmail.com>
2017-04-25 11:48:19 +0200

    Finish to build the project

--- rgb_apr24a/rgb_apr24a.ino
+++ rgb_apr24a/rgb_apr24a.ino
@@ -1,57 +1,47 @@
-//definiamo i pin ai quali collegare i piedini del LED RGB    
-const int ROSSO = 10;  
-const int VERDE = 9;  
-const int BLU = 8;
-int color = 0;
+const int RED = 10;  
+const int GREEN = 9;  
+const int BLUE = 8;
+int intensity = 0;
+char color;
 int i = 0;
+int c = 0;
   
 void setup() {
-  //impostazione i pin a cui sono collegadi i piedini del led RGB come uscite 
   Serial.begin(9600);
   Serial1.begin(9600);
-  pinMode(ROSSO, OUTPUT); 
-  pinMode(VERDE, OUTPUT); 
-  pinMode(BLU, OUTPUT);
-
-  analogWrite(VERDE, 255);
-  analogWrite(BLU, 255);
-  // inizializziamo a LOW i pin ROSSO, VERDE, BLU
-  // inizialmente il led RGB sarà spento 
-//  pulisci();
-//  colora(127,255,255);
+  pinMode(RED, OUTPUT); 
+  pinMode(GREEN, OUTPUT); 
+  pinMode(BLUE, OUTPUT);
+  digitalWrite(RED, LOW); 
+  digitalWrite(GREEN, LOW);  
+  digitalWrite(BLUE, LOW); 
 }  
   
-void loop() {    
-  
+void loop() {     
   if(Serial1.available()){
-//      Serial.print("A");
-      int c = Serial1.read();
-//      Serial.write(c);
-      color += c * pow(10,2 - i);
-//      Serial.println(color);
-      i += 1;
-//      String s = Serial1.readString();
-//      Serial.println(s);
-//      int i = Serial1.parseInt();
-//      Serial.println(i);  
+    if (i < 3) {
+      c = Serial1.read();
+      intensity += c * pow(10,(2 - i));
+    } else {
+      color = Serial1.read();
+    }
+    i += 1; 
   }  
-  if (i == 3) {
-    Serial.println(color  - 5441);
-    analogWrite(ROSSO, color  - 5441);
-    color = 0;
+  if (i == 4) {
+//    Serial.println(intensity - 5327);
+//    Serial.println(color);
+    switch (color) {
+      case 'r':
+        analogWrite(RED, intensity  - 5327);
+        break;
+      case 'g':
+        analogWrite(GREEN, intensity  - 5327);
+        break;
+      case 'b':
+        analogWrite(BLUE, intensity  - 5327);
+        break;
+    }
+    intensity = 0;
     i = 0;
   }
-//
-//  void colora(int R, int G, int B){
-//    analogWrite(ROSSO, R);
-//    analogWrite(VERDE, G);
-//    analogWrite(BLU, B);
-//  }
-//
-//  void pulisci(){
-//    digitalWrite(ROSSO, LOW); 
-//    digitalWrite(VERDE, LOW);  
-//    digitalWrite(BLU, LOW); 
-//  }
-
 }
